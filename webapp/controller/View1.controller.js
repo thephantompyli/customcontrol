201aad1d8f21cf1811a4bf350e8a52653b69d0cb
Vaisakh Satheesan <vaisakhsatheesan2017@gmail.com>
2026-01-11 19:04:07 +0000

    i nitial commit

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,20 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller","sap/m/MessageToast"
+], (Controller,MessageToast) => {
+    "use strict";
+
+    return Controller.extend("customcontrol.controller.View1", {
+        onInit() {
+        },
+        onAfterRendering(oEvent) {
+			this.byId("rating").reset();
+			
+		},
+        onRatingChange(oEvent) {
+			const fValue = oEvent.getParameter("value");
+			const oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
+
+			MessageToast.show(oResourceBundle.getText("ratingConfirmation", [fValue]));
+		}
+    });
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"customcontrol/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
